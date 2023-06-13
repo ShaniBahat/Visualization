fd47d53439d9bee7a7b803e68eebf652718a10d0
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-13 14:27:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
-import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import streamlit as st
+import altair as alt
 
 # Load the data into a pandas DataFrame
 df = pd.read_csv('/content/survey_lung_cancer.csv')
@@ -23,20 +22,20 @@ grouped_data = filtered_data.groupby(['AGE_GROUP', 'SMOKING']).size().unstack().
 # Create a new Streamlit app
 st.title('Count of Lung Cancer Cases by Age Group and Smoking Status')
 
-# Plot the line chart
-plt.plot(age_labels, grouped_data['Non-smoking'], marker='.', color='#267868', ms=10, label='Non-smoking')
-plt.plot(age_labels, grouped_data['Smoking'], marker='.', color='#68C4AD', ms=10, label='Smoking')
+# Convert the DataFrame to an Altair chart
+chart = alt.Chart(grouped_data.reset_index()).mark_line().encode(
+    x='AGE_GROUP',
+    y='Non-smoking',
+    color=alt.value('#267868'),
+    tooltip=['AGE_GROUP', 'Non-smoking']
+).properties(
+    title='Count of Lung Cancer Cases by Age Group and Smoking Status',
+    width=600,
+    height=400
+)
 
-plt.legend(title='Smoking Status', loc='upper right')
-plt.xlabel('Age Group')
-plt.ylabel('Count')
-
-# Convert the Matplotlib figure to an image
-fig_image = plt.gcf().canvas.tostring_rgb()
-plt.close()
-
-# Display the image in Streamlit
-st.image(fig_image, format='png')
+# Display the chart in Streamlit
+st.altair_chart(chart)
 
 # Display the DataFrame if desired
 st.write(grouped_data)
