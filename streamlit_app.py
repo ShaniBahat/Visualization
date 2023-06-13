87cd3c10879964e83e5e8e7b4c252617907c7914
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-13 18:31:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,14 +96,14 @@ chart = alt.Chart(graph_data).mark_bar().encode(
     x=alt.X('Symptom Count:O', axis=alt.Axis(title='Symptom Count'), sort=alt.EncodingSortField(field='Symptom Count', order='ascending')),
     y='Number of People:Q',
     color=alt.Color('SMOKING:N', scale=color_scale),
-    column='SMOKING:N',
+    column=alt.Column('SMOKING:N', header=alt.Header(labelOrient='bottom', title=None)),
     tooltip=['Symptom Count', 'Number of People'],
     opacity=alt.condition(
         alt.datum['SMOKING'] == 'Smoker',
         alt.value(1) if show_smoker else alt.value(0),
         alt.value(1) if show_non_smoker else alt.value(0)
     )
-).interactive()
+).resolve_scale(x='independent').interactive()
 
 # Display the graph
 st.altair_chart(chart, use_container_width=True)
