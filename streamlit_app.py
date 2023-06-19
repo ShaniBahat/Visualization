a51bb85b90f5b35021748a285b2c9f35cae038fb
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-19 22:41:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,11 @@ grouped_df = data.groupby(['Symptom Count', 'SMOKING', 'GENDER']).size().reset_i
 
 st.subheader('Number of Symptoms by Count of Patients')
 
+# Create the Plotly figure
+fig = go.Figure()
+
 # Filter by gender for the graph
 gender_filter = st.selectbox("Filter by Gender", ['All', 'M', 'F'])
-
-# Create the Plotly figure for plot 1
-fig1 = go.Figure()
 
 for smoking_type in grouped_df['SMOKING'].unique():
     temp_df = grouped_df[grouped_df['SMOKING'] == smoking_type]
@@ -30,49 +30,48 @@ for smoking_type in grouped_df['SMOKING'].unique():
     if gender_filter != 'All':
         temp_df = temp_df[temp_df['GENDER'] == gender_filter]
 
-    fig1.add_trace(go.Bar(
+    fig.add_trace(go.Bar(
         x=temp_df['Symptom Count'],
         y=temp_df['Number of People'],
         name=smoking_type
     ))
 
-# Update the layout for plot 1
-fig1.update_layout(
+# Update the layout
+fig.update_layout(
     xaxis_title='Number of Symptoms',
     yaxis_title='Count of Patients',
     barmode='group'
 )
 
-############### plot 2
+# Set x-axis tick labels for every number
+fig.update_xaxes(type='category')
 
-# Create the Plotly figure for plot 2
-fig2 = go.Figure()
+# Display the plot using Streamlit
+st.plotly_chart(fig)
+
+# Create the Plotly figure
+fig = go.Figure()
 
 df = data.groupby(['Age Group', 'SMOKING']).size().reset_index(name='Number of Cases')
 
 for smoking_type in df['SMOKING'].unique():
     temp_df = df[df['SMOKING'] == smoking_type]
     
-    fig2.add_trace(go.Scatter(
+    fig.add_trace(go.Scatter(
         x=temp_df['Age Group'],
         y=temp_df['Number of Cases'],
         mode='lines+markers',
         name=smoking_type
     ))
 
-# Update the layout for plot 2
-fig2.update_layout(
+# Update the layout
+fig.update_layout(
     title='Count of Lung Cancer Cases by Age Group and Smoking Status',
     xaxis_title='Age Group',
     yaxis_title='Number of Cases',
     legend=dict(title='Smoking Status')
 )
 
-# Display the plots using Streamlit
-col1, col2 = st.columns(2)
+# Display the plot using Streamlit
+st.plotly_chart(fig)
 
-with col1:
-    st.plotly_chart(fig1)
-
-with col2:
-    st.plotly_chart(fig2)
