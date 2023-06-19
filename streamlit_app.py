8b5358d44ec4225bbf3b72e74bbbcbe9144d56c1
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-19 21:55:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ with st.container():
         title='Count of Lung Cancer Cases by Age Group and Smoking Status',
         xaxis_title='Age Group',
         yaxis_title='Number of Cases',
-        legend=dict(title='Smoking Status', loc='upper right')
+        legend=dict(title='Smoking Status')
     )
 
     # Display the plot using Streamlit
