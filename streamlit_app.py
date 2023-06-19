5b98d035570db9367760439cb2c1a4c95aab0c2a
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-20 02:51:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,6 +130,6 @@ fig1.update_layout(title='Division of Cancer and Non-Cancer Cases - Smokers')
 fig2.update_layout(title='Division of Cancer and Non-Cancer Cases - Non-Smokers')
 
 # Display the pie charts side by side using Streamlit
-col1, col2 = st.beta_columns(2)
+col1, col2 = st.columns(2)
 col1.plotly_chart(fig1, use_container_width=True)
 col2.plotly_chart(fig2, use_container_width=True)
