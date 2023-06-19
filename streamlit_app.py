d91cd15331e97746ef8f64bb14d4e86389e3ef04
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-19 21:07:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import pandas as pd
 import altair as alt
 import plotly.graph_objects as go
 
-
 # Load the data
 data = pd.read_csv('survey_lung_cancer.csv')
 
+# Pre - proccess 
 age_bins = [30, 40, 50, 60, 70, 80, 90]
 
 data['Age Group'] = pd.cut(data['AGE'], bins=age_bins, labels=['31-40', '41-50', '51-60', '61-70', '71-80', '81-90'])
@@ -83,6 +83,37 @@ fig.update_layout(
     xaxis_title='Age Group',
     yaxis_title='Number of Cases',
     legend=dict(title='Smoking Status')
+)
+
+# Display the plot using Streamlit
+st.plotly_chart(fig)
+
+
+
+# Filter the data based on the desired condition
+filtered_df = data[data['GENDER'] == 'F']  # Update the condition as per your requirement
+
+# Group the filtered data by 'Symptom Count' and 'SMOKING' and calculate the count of people
+grouped_df = filtered_df.groupby(['Symptom Count', 'SMOKING'], as_index=False)['Number of People'].sum()
+
+# Create the Plotly figure
+fig = go.Figure()
+
+for smoking_type in grouped_df['SMOKING'].unique():
+    temp_df = grouped_df[grouped_df['SMOKING'] == smoking_type]
+    
+    fig.add_trace(go.Bar(
+        x=temp_df['Symptom Count'],
+        y=temp_df['Number of People'],
+        name=smoking_type
+    ))
+
+# Update the layout
+fig.update_layout(
+    title='Sign Violators Count by Symptom Count',
+    xaxis_title='Number of Symptoms',
+    yaxis_title='Count of People',
+    barmode='group'
 )
 
 # Display the plot using Streamlit
