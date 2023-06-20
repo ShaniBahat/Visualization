d183f886161fb2c684fc14db61b7075cb9588ae3
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-20 17:09:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,7 @@ fig = go.Figure()
 # Filter by gender for the graph
 gender_filter = st.selectbox("Filter by Gender", ['All', 'M', 'F'])
 
-colors = {'Non-Smoker': '#b3baba', 'Smoker': '#008f94'}
+colors = {'Non-Smoker': '#b3baba', 'Smoker': '#39b3b8'}
 
 for smoking_type in grouped_df['SMOKING'].unique():
     temp_df = grouped_df[grouped_df['SMOKING'] == smoking_type]
@@ -153,7 +153,7 @@ for smoking_type in grouped_df['SMOKING'].unique():
 fig.update_layout(
     xaxis_title='Number of Symptoms',
     yaxis_title='Count of Patients',
-    barmode='group'
+  #  barmode='group'
 )
 
 # Set x-axis tick labels for every number
