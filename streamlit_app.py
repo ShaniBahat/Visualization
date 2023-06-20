38e0e1bcc3923a7febe860bc9f6a7880ea2f16b5
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-20 13:24:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,7 +151,7 @@ fig.add_trace(go.Scatter(
     y=[y_value],
     mode='markers',
     name='Your Selection',
-    marker=dict(symbol='hexagon-dot', color='black', size=10)
+    marker=dict(symbol='hexagon-dot', color='black')
 ))
 ##############################################################
 
