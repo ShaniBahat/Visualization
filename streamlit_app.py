6aa4d6dd189f6151d28eecbcce23efa397089f0b
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-21 20:39:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ for i, symptom in enumerate(bubble_data['Symptom']):
         name=symptom,
         hovertemplate='<b>%{x}</b><br><br>' +
                       'Total Symptom Count: %{y}<br>'+
-                      'Cancer Cases: %{marker.size}<br>'
+                      'Cancer Cases: %{marker.size}'
     ))
 
 # Customize the layout
