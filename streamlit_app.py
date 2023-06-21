64fc956421013903054d48ddc426aa8813ef8270
ShaniBahat <107792183+ShaniBahat@users.noreply.github.com>
2023-06-21 18:10:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,8 @@ fig1.update_traces(marker=dict(colors=['#c6cccc', '#baddde']))
 # fig2.update_traces(marker=dict(colors=['#c6cccc', '#baddde']))
 
 # Display the pie charts side by side using Streamlit
-col1, col2 = st.columns(2)
-col1.plotly_chart(fig1, use_container_width=True)
+# col1, col2 = st.columns(2)
+st.plotly_chart(fig1, use_container_width=True)
 # col2.plotly_chart(fig2, use_container_width=True)
 
 #####################################################
